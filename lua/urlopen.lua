5d2b049a79f69c9f403c533deecbabd3021a6b04
Kristófer Fannar Björnsson <kristoferfannarb@gmail.com>
2024-07-02 00:45:33 +0000

    Initial commit - :UrlOpen finds current word

---
+++ lua/urlopen.lua
@@ -0,0 +1,48 @@
+print("in init.lua")
+
+local testurl = "https://google.com"
+
+local function is_valid_url(url)
+	local pattern = '^https?://[%w-_%.%?%.:/%+=&]*[^"]*$'
+	return string.match(url, pattern) ~= nil
+end
+
+local function get_word_under_cursor()
+	local col = vim.api.nvim_win_get_cursor(0)[2] + 1
+	local line = vim.api.nvim_get_current_line()
+
+	local left = col
+	while left > 0 and not line:sub(left, left):match("%s") do
+		left = left - 1
+	end
+	left = left + 1
+
+	local right = col
+	while right <= string.len(line) and not line:sub(right, right):match("%s") do
+		right = right + 1
+	end
+
+	if left == right then
+		return ""
+	end
+
+	return line:sub(left, right - 1)
+end
+
+local function open_url()
+	local word = get_word_under_cursor()
+	if is_valid_url(word) then
+		print("url: " .. word)
+	else
+		print('"' .. word .. '" is invalid url')
+	end
+end
+
+vim.api.nvim_create_user_command("UrlOpen", open_url, {})
+
+local M = {}
+M.test = function()
+	print("testing...")
+end
+
+return M
